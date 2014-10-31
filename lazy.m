2d69ba8c4234ef11a2f9e7c4f5457a1c8b7a66bb
David Walsh <dvdwlsh@gmail.com>
2014-10-31 03:58:35 -0400

    Added lazy creation

--- lazy.m
+++ lazy.m
@@ -1,11 +1,13 @@
-// If Statement
-// Execute code only when a certain condition is true.
+// Lazy Accessor
+// Create object when accessed or return existing
 //
-// IDECodeSnippetCompletionPrefix: if
+// IDECodeSnippetCompletionPrefix: lazy
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: D70E6D11-0297-4BAB-88AA-86D5D5CBBC5D
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetVersion: 1
-if (<#condition#>) {
-    <#statements#>
+-(<#instancetype#>)<#name#> {
+	if(!_<#name#>){
+    	<#creation#>
+    }
+    return _<#name#>
 }
